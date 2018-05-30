f5438d1737d576bc91d2ba903add910413604fb2
Marcel Ebert <s0558606@htw-berlin.de>
2018-05-30 18:08:43 +0200

    added menu to main activity added action to main activity menu item added dialog on menu item click

--- app/src/main/java/de/htw/berlin/s0558606/lasersensorcommunicator/MainActivity.kt
+++ app/src/main/java/de/htw/berlin/s0558606/lasersensorcommunicator/MainActivity.kt
@@ -5,13 +5,15 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
 import com.google.android.gms.maps.model.LatLng
 import de.htw.berlin.s0558606.lasersensorcommunicator.model.Location
 import de.htw.berlin.s0558606.lasersensorcommunicator.model.LocationViewModel
 import de.htw.berlin.s0558606.lasersensorcommunicator.ui.LocationAdapter
 import kotlinx.android.synthetic.main.content_main.*
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.warn
+import org.jetbrains.anko.*
 
 
 const val ARG_ITEM_ID = "item_id"
@@ -48,14 +50,41 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         rv_locations.setHasFixedSize(true)
         rv_locations.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
+    }
 
-        addNewLocation("Schöneweide", LatLng(10.0, 10.0))
-
+    private fun showAddLocationdialog() {
+        alert {
+            title = "Add new Location"
+            lateinit var etLocation: EditText
+            customView {
+                linearLayout {
+                    textView("Enter location name:")
+                    etLocation = editText("")
+                    padding = dip(16)
+                }
+            }
+            positiveButton("Cool") { addNewLocation(etLocation.text.toString(), LatLng(0.0, 0.0)) }
+        }.show()
     }
 
     private fun addNewLocation(name: String, location: LatLng) {
-        warn { "Function called" }
         mLocationViewModel.insert(Location(name, location))
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.add_location -> {
+                showAddLocationdialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
